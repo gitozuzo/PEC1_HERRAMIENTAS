44b988cbee7a66febd1e2c493bda00337185bdcf
Jorge Manchado Leal <jmanchadolea@uoc.edu>
2025-01-12 23:37:15 +0100

    Se modifica el formato de la imagenes en la página detalle

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  AOS.init();
+  // Verifica si estás en la página donde necesitas AOS
+  if (window.location.pathname.includes("detalle.html")) {
+    // Inicializa AOS solo en la página detalle.html
+    AOS.init();
+  }
 });
