3ff4e8fe21cbe8da386377c07bda68ab24fb0bc8
Jorge Manchado Leal <jmanchadolea@uoc.edu>
2025-01-13 12:21:53 +0100

    Se modifica el iframe de la página presentacion

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ console.log("1");
 document.addEventListener("DOMContentLoaded", () => {
   console.log("2");
   // Verifica si estás en la página donde necesitas AOS
-  if (window.location.pathname.includes("detalle.html")) {
+  if (window.location.pathname.includes("detalle")) {
     // Inicializa AOS solo en la página detalle.html
     console.log("3");
     AOS.init();
